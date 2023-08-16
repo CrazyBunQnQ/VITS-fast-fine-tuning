84963878ffee08ffc44f43702a42fd29c4f83deb
CrazyBunQnQ <baobao222222@qq.com>
2023-08-16 21:42:54 +0800

    update scripts

--- 预测一键启动.bat
+++ 预测一键启动.bat
@@ -1,5 +1,7 @@
 @echo off
 set path=.\venv\Scripts;.\venv;ffmpeg\bin;%path%
-.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_latest.pth --config_dir ./configs/modified_finetune_speaker.json --share False
+.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_3000.pth --config_dir ./configs/modified_finetune_speaker.json --share False
+::.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_latest.pth --config_dir ./OUTPUT_MODEL/config.json --share False
+::.\venv\python.exe VC_inference.py --model_dir F:/AI/TrainingData/VITS-fast-fine-tuning/OUTPUT_MODEL/G_latest.pth --config_dir F:/AI/TrainingData/VITS-fast-fine-tuning/configs/modified_finetune_speaker.json --share False
 ::.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_latest.pth --config_dir ./finetune_speaker.json --share False
 pause
