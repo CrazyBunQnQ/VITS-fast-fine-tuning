2a667659fc608cc51ff1de60f26130c3ccc5fba4
CrazyBunQnQ <baobao222222@qq.com>
2023-08-19 03:17:56 +0800

    nothing

--- 预测一键启动.bat
+++ 预测一键启动.bat
@@ -4,7 +4,11 @@ set path=.\venv\Scripts;.\venv;ffmpeg\bin;%path%
 .\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_latest.pth --config_dir ./configs/modified_finetune_speaker.json --share False
 
 :: custom
-::.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_3000.pth --config_dir ./configs/modified_finetune_speaker.json --share False
+::.\venv\python.exe VC_inference.py --model_dir ./OUTPUT_MODEL/G_530.pth --config_dir ./configs/modified_finetune_speaker.json --share False
 ::.\venv\python.exe VC_inference.py --model_dir F:/AI/TrainingData/VITS-fast-fine-tuning/OUTPUT_MODEL/G_latest.pth --config_dir F:/AI/TrainingData/VITS-fast-fine-tuning/configs/modified_finetune_speaker.json --share False
 ::.\venv\python.exe VC_inference.py --model_dir ./pretrained_models/G_trilingual.pth --config_dir ./configs/uma_trilingual.json --share False
+
+:: release
+:: kafka
+::.\venv\python.exe VC_inference.py --model_dir ./pretrained_models/G_kafka_4060.pth --config_dir ./pretrained_models/G_kafka_config.json --share False
 pause
